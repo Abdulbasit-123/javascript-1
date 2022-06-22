a71ec311454523ff5504bcf90575cf92dabdbca6
Abdulbasit-123 <bodundeabdulbasit802@gmail.com>
2022-06-22 07:56:15 -0400

    initial commit

---
+++ script.js
@@ -0,0 +1,7 @@
+const Name = "Bodunde Abdulbasit";
+const Height = "5feet 7inches";
+const Country = "Nigeria";
+
+alert("My name is " + Name)
+alert("My height is " + Height)
+alert("My country is " + Country)
